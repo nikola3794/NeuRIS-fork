5dad77427fed751bc65de9c973d67fa7e0e58911
Nikola Popovic <nipopovic@archer.ee.ethz.ch>
2023-05-29 12:54:40 +0200

    ...

--- submit_jobs/train.sh
+++ submit_jobs/train.sh
@@ -11,34 +11,16 @@
 
 ########SBATCH --gpus=rtx_2080_ti:1
 
-module load python_gpu/3.7.4
-PYTHONPATH=
-export PYTHONPATH
+#module load python_gpu/3.7.4
+#PYTHONPATH=
+#export PYTHONPATH
 source /cluster/home/nipopovic/python_envs/neuris/bin/activate
-
-# Trick to prevent crashes due to matplotlib
-DISPLAY=
-export DISPLAY
-
-mkdir ${TMPDIR}/dimitrios_gaze/
-rsync -aPq /cluster/work/cvl/specta/data/dimitrios_gaze/ ${TMPDIR}/dimitrios_gaze
-ls ${TMPDIR}
-ls ${TMPDIR}/dimitrios_gaze
 
 nvidia-smi
 
-cd /cluster/project/cvl/specta/code/3D-eye-model-seg/
+cd /cluster/project/cvl/specta/code/NeuRIS-fork
 
-B=1
-F=10
-MODEL=DenseEl3
-N_ang=72
-N_rad=8
 
-# 'OpenEDS_S' 'TEyeD'
-#--net_ellseg_head \
-#--net_simply_head \
-#--loss_rend_vectorized \
-/cluster/home/nipopovic/python_envs/ellseg/bin/python -u run.py \
+/cluster/home/nipopovic/python_envs/neuris/bin/python -u exp_runner.py 
 
 
